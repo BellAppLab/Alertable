7be4fd4bf770913b8deeb71ecceffafec87ccb66
Bell App Lab <apps@bellapplab.com>
2016-10-06 13:20:59 +0100

    Oh god

---
+++ Example/Pods/Target Support Files/Pods-Alertable_Example/Pods-Alertable_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Alertable_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Alertable_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-Alertable_Tests/Pods-Alertable_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Alertable_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Alertable_Tests
+@end
